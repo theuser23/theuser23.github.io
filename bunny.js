9661e3274cdffd75ab5893e3f4295806fb90c54a
theuser23 <theuser23@users.noreply.github.com>
2016-08-09 00:16:47 -0500

    Delete bunny.js

--- bunny.js
+++
@@ -1,7 +0,0 @@
-var bunnyProject = angular.module('bunnyProject', []);
-bunnyProject.controller('bunnyController', function($scope) {
-	$scope.things = ["Watch You're the Worst", "Watch Crazy Ex-Girlfriend", "Call Mom", "Cry", "Study"];
-	$scope.clickMe = function() {
-	$scope.randomThing = $scope.things[Math.floor(Math.random() * 5)];
-	}
-});
